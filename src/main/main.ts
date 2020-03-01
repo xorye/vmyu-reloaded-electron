ef8d05c17c9ad2c5e7ad517f3d1cbccc1c8be7c9
David <kwondavid98@gmail.com>
2020-02-29 22:48:46 -0500

    Remove unnecessary files

--- src/main/main.ts
+++ src/main/main.ts
@@ -19,7 +19,7 @@ const createWindow = async () => {
         await installExtensions();
     }
 
-    win = new BrowserWindow({ width: 800, height: 600 });
+    win = new BrowserWindow({ width: 360, height: 600 });
 
     if (process.env.NODE_ENV !== 'production') {
         process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1'; // eslint-disable-line require-atomic-updates
@@ -37,7 +37,7 @@ const createWindow = async () => {
     if (process.env.NODE_ENV !== 'production') {
         // Open DevTools, see https://github.com/electron/electron/issues/12438 for why we wait for dom-ready
         win.webContents.once('dom-ready', () => {
-            win!.webContents.openDevTools();
+            // win!.webContents.openDevTools();
         });
     }
 
