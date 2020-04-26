1a97696b3bf7ff2a2618bcbd3612e9d744ad14a7
David Kwon <kwondavid98@gmail.com>
2020-04-26 17:05:52 -0400

    Got search state working

--- src/renderer/components/SearchBar.tsx
+++ src/renderer/components/SearchBar.tsx
@@ -1,12 +1,52 @@
 import * as React from 'react';
 import './css/SearchBar.css';
+import { StoreInterface } from '../store/store';
+import { connect } from 'react-redux';
+import { searchQueryChanged } from '../store/nav/actions';
 
-class SearchBar extends React.Component {
+interface IProps {
+    searchQueryChanged: (str: string) => any;
+}
+
+interface IState {
+    inputValue: string;
+}
+
+class SearchBar extends React.Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+        this.state = { inputValue: '' };
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.updateInputValue = this.updateInputValue.bind(this);
+    }
+
+    handleKeyDown(e: React.KeyboardEvent) {
+        if (e.key === 'Enter') {
+            this.props.searchQueryChanged(this.state.inputValue);
+        }
+    }
+
+    updateInputValue(evt: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            inputValue: evt.target.value
+        });
+    }
+
     render() {
         return <div className='SearchBar'>
-            <input className='SearchBar__input' type='text' placeholder='Search words'></input>
+            <input className='SearchBar__input' type='text' placeholder='Search words'
+                onKeyDown={this.handleKeyDown}
+                onChange={this.updateInputValue}
+            ></input>
         </div>
     }
 }
 
-export default SearchBar
+const mapStateToProps = (state: StoreInterface) => ({
+    sideBarOpened: state.navStore.sideBarOpened,
+    currentView: state.currentView.view
+});
+
+export default connect(mapStateToProps, {
+    searchQueryChanged
+})(SearchBar);
--- src/renderer/components/SideBar.tsx
+++ src/renderer/components/SideBar.tsx
@@ -1,17 +1,36 @@
 import * as React from 'react';
 import './css/SideBar.css';
 import SideBarEntry from './SideBarEntry';
+import { changeView } from '../store/currentView/actions';
+import { clearWords } from '../store/words/actions';
+import { clearPages } from '../store/pages/actions';
 import { StoreInterface } from '../store/store';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
+import { ViewEnum } from '../store/currentView/types';
 
 interface IProps {
+    changeView: (newView: ViewEnum) => any;
+    clearWords: () => any;
+    clearPages: () => any;
     sideBarOpened: boolean;
+    currentView: ViewEnum;
 }
 
 export class SideBar extends React.Component<IProps> {
     constructor(props: IProps) {
         super(props);
+        this.createChangeView = this.createChangeView.bind(this);
+    }
+    createChangeView(view: ViewEnum) {
+        return () => {
+            if (this.props.currentView === view) {
+                return;
+            }
+            this.props.clearWords();
+            this.props.clearPages();
+            this.props.changeView(view);
+        }
     }
     render() {
 
@@ -21,15 +40,19 @@ export class SideBar extends React.Component<IProps> {
         });
 
         return <div className={classes}>
-            <SideBarEntry text='Pages' />
-            <SideBarEntry text='Words' />
-            <SideBarEntry text='Data' />
+            <SideBarEntry text='Pages' isSelected={this.props.currentView === ViewEnum.PAGES} onClick={this.createChangeView(ViewEnum.PAGES)} />
+            <SideBarEntry text='Words' isSelected={this.props.currentView === ViewEnum.WORDS} onClick={this.createChangeView(ViewEnum.WORDS)} />
+            <SideBarEntry text='Data' isSelected={false} onClick={() => { }} />
         </div>
     }
 }
-
 const mapStateToProps = (state: StoreInterface) => ({
-    sideBarOpened: state.navStore.sideBarOpened
+    sideBarOpened: state.navStore.sideBarOpened,
+    currentView: state.currentView.view
 });
 
-export default connect(mapStateToProps, {})(SideBar);
+export default connect(mapStateToProps, {
+    changeView,
+    clearWords,
+    clearPages
+})(SideBar);
