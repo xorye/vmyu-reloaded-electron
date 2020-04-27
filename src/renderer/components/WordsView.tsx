39c1e7502d307cda82dc2eef6ee24e72898ff551
David Kwon <kwondavid98@gmail.com>
2020-04-27 18:38:17 -0400

    Use Masonry for word view

--- src/renderer/components/WordsView.tsx
+++ src/renderer/components/WordsView.tsx
@@ -5,25 +5,36 @@ import { Word } from '../types';
 import WordEntry from './WordEntry';
 import { addWords, changeWordsByUrl } from '../store/words/actions';
 import './css/WordsView.css';
+import Masonry from 'react-masonry-component';
 
-interface WordsViewProps {
+interface IProps {
     addWords: (words: Word[]) => any;
     changeWordsByUrl: (url: string) => any;
     words: Word[];
     currentUrl: string | undefined;
 }
 
-export class WordsView extends React.Component<WordsViewProps> {
+export class WordsView extends React.Component<IProps> {
+
+    constructor(props: IProps) {
+        super(props);
+    }
+
     async componentDidMount() {
         if (this.props.currentUrl) {
             await this.props.changeWordsByUrl(this.props.currentUrl);
         }
     }
     render() {
+        const masonryOptions: Masonry.MasonryOptions = {
+            horizontalOrder: true,
+            gutter: 10,
+            fitWidth: true
+        }
         const resultEntries = this.props.words
             .sort((wordA, wordB) => wordA.timestamp!.getTime() - wordB.timestamp!.getTime())
             .map((word: Word) => <WordEntry word={word} />);
-        return <div className="WordsView">{resultEntries}</div>;
+        return <Masonry className='WordsView' options={masonryOptions}>{resultEntries}</Masonry>;
     }
 }
 
