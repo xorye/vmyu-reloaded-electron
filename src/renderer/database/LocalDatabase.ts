c35fa66c9856fe76164028360fdae14c38e7f25d
David Kwon <kwondavid98@gmail.com>
2020-06-10 18:13:07 -0400

    Update types

--- src/renderer/database/LocalDatabase.ts
+++ src/renderer/database/LocalDatabase.ts
@@ -165,7 +165,7 @@ export class LocalDatabase implements IDatabase {
         return results.data.id;
       });
   }
-  commonWords(userId: number, words: string[], similarityConstant?: number): Promise<CommonWordsResult> {
+  commonWords(userId: number, words: string[], similarityConstant?: number): Promise<CommonWordsResult[]> {
     return axios
       .post(`${URL}/commonWords/`, {
         userId,
