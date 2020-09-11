3974e7b47c2fc8e2efefc9538aab63aaa2a99b73
David <davidkwon@Davids-MacBook-Pro.local>
2020-09-11 11:28:14 -0400

    Define REFRESH constant

--- src/main/socketServer.ts
+++ src/main/socketServer.ts
@@ -2,18 +2,17 @@ import * as express from 'express';
 import * as socket from 'socket.io';
 import { BrowserWindow } from 'electron';
 
+export const REFRESH: string = 'REFRESH';
+
 export function onConnection(win: BrowserWindow) {
-    console.log('sock');
     const app = express();
     const server = app.listen(4000, () => {
         console.log('listening to requests on 4000');
     });
-
     const io = socket(server, { serveClient: false });
     io.on('connection', (socket: any) => {
-        console.log('made socket connection', socket.id);
-        socket.on('REFRESH', (data: any) => {
-            win.webContents.send('REFRESH', data);
+        socket.on(REFRESH, (data: any) => {
+            win.webContents.send(REFRESH, data);
         });
     });
 }
